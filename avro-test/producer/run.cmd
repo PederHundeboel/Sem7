d2d6cb382bbd436041b5e84ea5f81c2bb4bc2506
Loc Hoang Thanh Nguyen <vlocnguyen@live.dk>
2021-11-12 07:47:34 +0100

    test

--- avro-test/producer/run.cmd
+++ avro-test/producer/run.cmd
@@ -3,3 +3,5 @@ docker run --network hadoop producer
 
 :: stop/remove all;  docker stop $(docker ps -aq) && docker rm $(docker ps -aq) && docker volume rm kafka_volume
 :: start hdfs-connect-sink; curl -s -X POST -H 'Content-Type: application/json' --data @hdfs-sink.json http://localhost:8083/connectors
+
+curl -s -X POST -H 'Content-Type: application/json' --data @hbase-sink.json http://localhost:8083/connectors
