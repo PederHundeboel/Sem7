3ff164e5b838d77ee26bc189ca4dc29ae1dab961
Loc Hoang Thanh Nguyen <vlocnguyen@live.dk>
2021-11-15 22:06:40 +0100

    hbase sink works in the setup now

--- avro-test/producer/avro-producer.py
+++ avro-test/producer/avro-producer.py
@@ -5,17 +5,18 @@ from confluent_kafka.avro import AvroProducer
 
 value_schema = avro.load('/usr/src/app/schema/ValueSchema.avsc')
 key_schema = avro.load('/usr/src/app/schema/KeySchema.avsc')
-key = {"name": "loc"}
+key = {"name": ""}
 value = {"name": "Value", "favorite_number": 10, "favorite_color": "green", "age": 25}
+value2 = {"name" : "test_meter", "type" : "meter", "timestamp" : 1574667646013, "dimensions" : {"dimensions1" : "InstanceID","dimensions2" : "i-aaba32d4"},"values" : {"count" : 32423.0,"oneMinuteRate" : 342342.2,"fiveMinuteRate" : 34234.2,"fifteenMinuteRate" : 2123123.1,"meanRate" : 2312312.1}}
 
 avroProducer = AvroProducer(
     {'bootstrap.servers': 'kafka:19092', 'schema.registry.url': 'http://schema-registry:8082'},
     default_key_schema=key_schema, default_value_schema=value_schema
     )
 
-for i in range(0, 50):
-    value = {"name": "nguyen", "favorite_number": 10, "favorite_color": "green", "age": i}
-    avroProducer.produce(topic='my_topic', value=value, key=key, key_schema=key_schema, value_schema=value_schema)
+for i in range(0, 20):
+    #value = {"name": "nguyen", "favorite_number": 10, "favorite_color": "green", "age": i}
+    avroProducer.produce(topic='products', value=value2, key={"name": str(i)}, key_schema=key_schema, value_schema=value_schema)
     sleep(0.01)
     print(i)
 
--- avro-test/producer/run.cmd
+++ avro-test/producer/run.cmd
@@ -1,7 +1,7 @@
 docker build . -t producer:latest 
 docker run --network hadoop producer
 
-:: stop/remove all;  docker stop $(docker ps -aq) && docker rm $(docker ps -aq) && docker volume rm kafka_volume
-:: start hdfs-connect-sink; curl -s -X POST -H 'Content-Type: application/json' --data @hdfs-sink.json http://localhost:8083/connectors
+:: stop/remove all:             docker stop $(docker ps -aq) && docker rm $(docker ps -aq) && docker volume rm kafka_volume
+:: start hdfs-connect-sink:     curl -s -X POST -H 'Content-Type: application/json' --data @hdfs-sink.json http://localhost:8083/connectors
+:: start hsbase sink:           curl -s -X POST -H 'Content-Type: application/json' --data @hbase-sink.json http://localhost:8083/connectors
 
-curl -s -X POST -H 'Content-Type: application/json' --data @hbase-sink.json http://localhost:8083/connectors
