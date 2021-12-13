51765ac26d5888f211b8a2b003eb0c652f090151
Loc Hoang Thanh Nguyen <vlocnguyen@live.dk>
2021-12-13 17:01:38 +0100

    avro hbase stuff

--- avro-test/hbase-consumer/hbase-consumer.py
+++ avro-test/hbase-consumer/hbase-consumer.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 #import pdhbase as pdh
 import pdhhbase as pdh
+import json 
+
 connection = None
 try:
     connection = happybase.Connection('hbase')
@@ -10,16 +12,22 @@ try:
 
     #print(connection.tables())
     table = connection.table('products-avro')
-    row = table.row('c1c8736ada6a457abd4af68ec70a773a')
-    print(row[b'dimensions:dimensions1'])  # prints 'value1'
-    print(row)   # prints the value of cf1:col1
+    row = table.row('adf7c8d7c7ea45a5aa6be354a8f13d61')
+    #print(row)
+
+    coordinates = json.dumps(row[b'products:features'])
+    print(coordinates)
+
+    id = row[b'products:id'].decode("utf-8")
+    print(id)
+    #print(row[b'products:shape'])  # prints 'value1'
     #for key, data in table.scan():
     #    print(key, data)
 
 
     # test
-    df = pdh.read_hbase(connection, 'products-avro', '', cf=b'dimensions:dimensions1')
-    print(df)
+    #df = pdh.read_hbase(connection, 'products-avro', '', cf=b'dimensions:dimensions1')
+    #print(df)
 finally:
     if connection:
         connection.close()
--- avro-test/producer/run.cmd
+++ avro-test/producer/run.cmd
@@ -4,4 +4,5 @@ docker run --network hadoop producer
 :: stop/remove all:             docker stop $(docker ps -aq) && docker rm $(docker ps -aq) && docker volume rm kafka_volume
 :: start hdfs-connect-sink:     curl -s -X POST -H 'Content-Type: application/json' --data @hdfs-sink.json http://localhost:8083/connectors
 :: start hsbase sink:           curl -s -X POST -H 'Content-Type: application/json' --data @hbase-sink.json http://localhost:8083/connectors
+:: delete schema:               curl -X DELETE http://localhost:8082/subjects/products-value
 
