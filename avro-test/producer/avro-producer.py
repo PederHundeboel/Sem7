4adce29f764a800be85c2fa82c45ceef141b4dce
Loc Hoang Thanh Nguyen <vlocnguyen@live.dk>
2021-12-20 11:41:17 +0100

    cleanup

--- avro-test/producer/avro-producer.py
+++ avro-test/producer/avro-producer.py
@@ -5,7 +5,7 @@ from confluent_kafka.avro import AvroProducer
 import uuid
 import json
 
-value_schema = avro.load('/usr/src/app/schema/ValueSchema4.avsc')
+value_schema = avro.load('/usr/src/app/schema/ValueSchema.avsc')
 key_schema = avro.load('/usr/src/app/schema/KeySchema.avsc')
 key = {"name": ""}
 
@@ -17,13 +17,6 @@ avroProducer = AvroProducer(
     {'bootstrap.servers': 'kafka:19092', 'schema.registry.url': 'http://schema-registry:8082', 'compression.type': 'snappy', 'message.max.bytes': 15728640},
     default_key_schema=key_schema, default_value_schema=value_schema
     )
-
-for i in range(0, 1):
-    key = uuid.uuid4().hex
-    #value = {"name": "nguyen", "favorite_number": 10, "favorite_color": "green", "age": i}
-    #avroProducer.produce(topic='products', value=value2, key={"name": str(i)}, key_schema=key_schema, value_schema=value_schema)
-    avroProducer.produce(topic='products', value=value, key={"name": key}, key_schema=key_schema, value_schema=value_schema)
-    sleep(0.01)
-   # print(i)
-
+key = uuid.uuid4().hex
+avroProducer.produce(topic='products', value=value, key={"name": key}, key_schema=key_schema, value_schema=value_schema)
 avroProducer.flush()
